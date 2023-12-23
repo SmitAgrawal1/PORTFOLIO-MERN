c3893338e2c2ee985589875a803eb7b4da6bad8e
SmitAgrawal1 <smitagrawal456@gmail.com>
2023-12-24 02:40:24 +0530

    portfolio app
    mern stack portfolio app

---
+++ controllers/portfolioController.js
@@ -0,0 +1,65 @@
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
+
+//transport
+const transporter = nodemailer.createTransport(
+  sendGridTransport({
+    auth: {
+      api_key: process.env.API_SENDGRID,
+    },
+  })
+);
+
+const sendEmailController = (req, res) => {
+  try {
+    const { name, email, msg } = req.body;
+
+    //validation
+    if (!name || !email || !msg) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide All Fields",
+      });
+    }
+    //email matter
+    transporter.sendMail({
+      to: "smitagrawal456@gmail.com",
+      from: "smitagrawal456@gmail.com",
+      subject: "Regarding Mern Portfolio App",
+      html: `
+        <h5>Detail Information</h5>
+        <ul>
+          <li><p>Name : ${name}</p></li>
+          <li><p>Email : ${email}</p></li>
+          <li><p>Message : ${msg}</p></li>
+        </ul>
+      `,
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Your Message Send Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Send Email API Error",
+      error,
+    });
+  }
+};
+
+module.exports = { sendEmailController };
+
+
+
+
+
+
+
+
+
+// d53268588b919a1873c4e791a33820e3-5e3f36f5-383af416
+
+// 230a7f6df3aa6e6babc7fae5558c3953-5e3f36f5-1b19ca3a
